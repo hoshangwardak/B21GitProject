7fe619483a365011000e2ed0e0cc69b813cbe0bd
hoshangwardak <hoshangwardak@gmail.com>
2020-11-11 21:56:32 -0500

    Project is already shared and committing and pushing more changes

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     // Mor Stuff
     // Mach users and windows users are happy
-
+    // I have already published
+    // on GitHub.com now I am ready to make more commit
+    // But do I need to share again
+    // Absolutely no
+    // Its time to commit and just Push
 }
